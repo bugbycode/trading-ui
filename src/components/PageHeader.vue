65e0657cf4a46b61ed4a7e29e4439dcadbab8416
bugbycode <bugbycode@gmail.com>
2024-11-25 21:59:43 +0800

    价格行为分析变更为交易机会分析

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -62,7 +62,7 @@
                     <el-radio-button label="关闭" :value="0"/>
                 </el-radio-group>
             </el-form-item>
-            <el-form-item label="价格行为分析" :label-width="settingLabelWidth" >
+            <el-form-item label="交易机会分析" :label-width="settingLabelWidth" >
                 <el-radio-group v-model="settingForm.emaMonitor" size="small">
                     <el-radio-button label="开启" :value="1" />
                     <el-radio-button label="关闭" :value="0"/>
